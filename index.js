740434d84e6e99cbb23ad82cb0b647e3f6a93632
bingo <blackbing@gmail.com>
2014-04-28 19:20:08 +0800

    fixed script

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ module.exports = function() {
   return streamMap(function(file, cb) {
     child_process.exec('git rev-parse HEAD', function (error ,stdout) {
       var string = [
+        '\n',
         '// git version:' + stdout,
         '// create at: ' + new Date()
-      ].join('\n');
+      ].join('');
       file.contents = new Buffer(file.contents.toString() + string);
       cb(null, file);
-    }
+    });
   });
 };
 
