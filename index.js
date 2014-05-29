3b252bbd04acdd54d8eab74f0b883d45ac43c960
bingo <blackbing@gmail.com>
2014-05-29 15:09:49 +0800

    add type option

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 var streamMap = require('map-stream');
 var child_process = require('child_process');
-module.exports = function() {
+
+
+var trim=function(s){return s.replace(/^\s+|\s+$/g, '');};
+module.exports = function(opts) {
+
+  var d = new Date();
+  var WrapTypes = {
+    'js': function(v){
+      return ";\n// git version: " + v + "\n// created at: " + d;
+    },
+    'css': function(v){
+      return "/* git version: " + v + " */\n/* created at: " + d + "*/";
+    },
+    'html': function(v){
+      return "<!-- git version: " + v + " -->" + "\n<!-- created at: " + d + " -->";
+    }
+  };
+
+  if(!opts){
+    opts = {}
+    opts.type = 'js'
+  }
+  wrapType = WrapTypes[opts.type];
+  if(!wrapType){
+    console.error();
+    errMsg = '[gulp-gitversion] Not support this type: ['+ opts.type +']!';
+    errMsg += '\nPlease contact blackbing@gmail.com if you have any suggestion';
+    throw new Error(errMsg);
+  }
+
   return streamMap(function(file, cb) {
     child_process.exec('git rev-parse HEAD', function (error ,stdout) {
-      var string = [
-        ';\n',
-        '// git version:' + stdout,
-        '// create at: ' + new Date()
-      ].join('');
+      var v = trim(stdout);
+      var string = wrapType(v);
       file.contents = new Buffer(file.contents.toString() + string);
       cb(null, file);
     });
